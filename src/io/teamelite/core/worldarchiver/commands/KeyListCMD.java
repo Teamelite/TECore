0b0452d10ea15f4e944b0786f9ae41c5fac00195
Neal <n.asprion@gmx.de>
2015-12-08 14:28:11 +0100

    Bug fix
    Adding onCommand()

--- src/io/teamelite/core/worldarchiver/commands/KeyListCMD.java
+++ src/io/teamelite/core/worldarchiver/commands/KeyListCMD.java
@@ -5,21 +5,24 @@ import io.teamelite.core.worldarchiver.WorldArchiverMain;
 
 public class KeyListCMD implements CommandExecutor{
 	
-	if(cmd.getName().equalsIgnoreCase("key-list")){
-		if(sender.hasPermission("worldarchive.keys")){
-			if(WorldArchiverMain.files.keySet().size()==0){
-				sender.sendMessage("[TE-Core: World-Archiver] There are no keys set.");
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("key-list")){
+			if(sender.hasPermission("worldarchive.keys")){
+				if(WorldArchiverMain.files.keySet().size()==0){
+					sender.sendMessage("[TE-Core: World-Archiver] There are no keys set.");
+					return true;
+				}
+				sender.sendMessage("[TE-Core: World-Archiver] Keys:");
+				for(String s : WorldArchiverMain.files.keySet()){
+					sender.sendMessage("- "+s);
+				}
+				return true;
+			} else{
+				sender.sendMessage("[TE-Core: World-Archiver] You do not have the permission to use this command!");
 				return true;
 			}
-			sender.sendMessage("[TE-Core: World-Archiver] Keys:");
-			for(String s : WorldArchiverMain.files.keySet()){
-				sender.sendMessage("- "+s);
-			}
-			return true;
-		} else{
-			sender.sendMessage("[TE-Core: World-Archiver] You do not have the permission to use this command!");
-			return true;
 		}
+		return false;
 	}
 	
 }
--- src/io/teamelite/core/worldarchiver/commands/WorldArchiveCMD.java
+++ src/io/teamelite/core/worldarchiver/commands/WorldArchiveCMD.java
@@ -15,80 +15,82 @@ import io.teamelite.core.worldarchiver.WorldArchiverMain;
 
 public class WolrdArchiveCMD implements CommandExecutor{
 	
-	if(cmd.getName().equalsIgnoreCase("world-archive")){
-		if(sender.hasPermission("worldarchive.archive")){
-			String name = null;
-			String key = null;
-			if(args.length==0 && sender instanceof Player){
-				name = ((Player)sender).getWorld().getName();
-				key = name;
-			} else if(args.length==1){
-				name = args[0];
-				key = name;
-			} else if(args.length>=2){
-				name = args[0];
-				key = args[1];
-			}
-			if(name==null||key==null){
-				return false;
-			}
-			File world = new File(name);
-			if(!(world.exists())){
-				sender.sendMessage("[TE-Core: World-Archiver] The world does not exist!");
-				return false;
-			}
-			if(WorldArchiverMain.files.keySet().contains(key)){
-				sender.sendMessage("[TE-Core: World-Archiver] Error: Duplication of a key!");
-				return false;
-			}
-			for(String filekey : WorldArchiverMain.files.keySet()){
-				if(WorldArchiverMain.files.get(filekey).getName().equals(world.getName())){
-					sender.sendMessage("[TE-Core: World-Archiver] Error: There is already an archived world with the same name!");
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("world-archive")){
+			if(sender.hasPermission("worldarchive.archive")){
+				String name = null;
+				String key = null;
+				if(args.length==0 && sender instanceof Player){
+					name = ((Player)sender).getWorld().getName();
+					key = name;
+				} else if(args.length==1){
+					name = args[0];
+					key = name;
+				} else if(args.length>=2){
+					name = args[0];
+					key = args[1];
+				}
+				if(name==null||key==null){
 					return false;
 				}
-			}
-			World w = this.getServer().getWorld(name);
-			World fallback = this.getServer().getWorld(this.getConfig().getString("WorldArchive.fallback"));
-			if(fallback==null){
-				sender.sendMessage("[TE-Core: World-Archiver] Error: The fallback world does not exist!");
+				File world = new File(name);
+				if(!(world.exists())){
+					sender.sendMessage("[TE-Core: World-Archiver] The world does not exist!");
+					return false;
+				}
+				if(WorldArchiverMain.files.keySet().contains(key)){
+					sender.sendMessage("[TE-Core: World-Archiver] Error: Duplication of a key!");
+					return false;
+				}
+				for(String filekey : WorldArchiverMain.files.keySet()){
+					if(WorldArchiverMain.files.get(filekey).getName().equals(world.getName())){
+						sender.sendMessage("[TE-Core: World-Archiver] Error: There is already an archived world with the same name!");
+						return false;
+					}
+				}
+				World w = this.getServer().getWorld(name);
+				World fallback = this.getServer().getWorld(this.getConfig().getString("WorldArchive.fallback"));
+				if(fallback==null){
+					sender.sendMessage("[TE-Core: World-Archiver] Error: The fallback world does not exist!");
+					return true;
+				}
+				if(w.equals(fallback)){
+					sender.sendMessage("[TE-Core: World-Archiver] You can not archive the fallback server.");
+					return false;
+				}
+				List<Player> pl = w.getPlayers();
+				ArrayList<Location> ll = new ArrayList<Location>();
+				for(Player p : pl){
+					p.sendMessage("[TE-Core: WorldArchive] This world has to be reloaded. You will be teleported back shortly!");
+					ll.add(p.getLocation());
+					p.teleport(fallback.getSpawnLocation());
+				}
+				w.save();
+				this.getServer().unloadWorld(w, true);
+				File worldarchive = new File(this.getDataFolder()+File.separator+name);
+				try{
+					Utils.copyFile(world, worldarchive);
+				} catch(IOException e){
+					sender.sendMessage("[TE-Core: World-Archiver] An error occured. Please check the console for further information.");
+					e.printStackTrace();
+					return true;
+				}
+				this.getServer().createWorld(new WorldCreator(name));
+				int x = 0;
+				for(Player p : pl){
+					p.teleport(ll.get(x));
+					x++;
+				}
+				WorldArchiverMain.files.put(key, worldarchive);
+				WorldArchiverMain.writekeys();
+				sender.sendMessage("[TE-Core: World-Archiver] Successfully archived the world. The key is: "+key);
+				return true;
+			} else{
+				sender.sendMessage("[TE-Core: World-Archiver] You do not have the permission to use this command!");
 				return true;
 			}
-			if(w.equals(fallback)){
-				sender.sendMessage("[TE-Core: World-Archiver] You can not archive the fallback server.");
-				return false;
-			}
-			List<Player> pl = w.getPlayers();
-			ArrayList<Location> ll = new ArrayList<Location>();
-			for(Player p : pl){
-				p.sendMessage("[TE-Core: WorldArchive] This world has to be reloaded. You will be teleported back shortly!");
-				ll.add(p.getLocation());
-				p.teleport(fallback.getSpawnLocation());
-			}
-			w.save();
-			this.getServer().unloadWorld(w, true);
-			File worldarchive = new File(this.getDataFolder()+File.separator+name);
-			try{
-				Utils.copyFile(world, worldarchive);
-			} catch(IOException e){
-				sender.sendMessage("[TE-Core: World-Archiver] An error occured. Please check the console for further information.");
-				e.printStackTrace();
-				return true;
-			}
-			this.getServer().createWorld(new WorldCreator(name));
-			int x = 0;
-			for(Player p : pl){
-				p.teleport(ll.get(x));
-				x++;
-			}
-			WorldArchiverMain.files.put(key, worldarchive);
-			WorldArchiverMain.writekeys();
-			sender.sendMessage("[TE-Core: World-Archiver] Successfully archived the world. The key is: "+key);
-			return true;
-		} else{
-			sender.sendMessage("[TE-Core: World-Archiver] You do not have the permission to use this command!");
-			return true;
 		}
-		
+		return false;
 	}
 	
 }
