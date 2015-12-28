4de5b0a16b7fe8d98395c86458b8bff05c71afaf
[LIAM] <liam@axanite.net>
2015-12-28 13:54:52 +0000

    omg im so done

--- src/io/teamelite/core/utilities/Utils.java
+++ src/io/teamelite/core/utilities/Utils.java
@@ -25,45 +25,4 @@ public class Utils {
 		return p;
 	}
 }
-	
-	
-	//to copy a file to another place
-	public static void copyFile(File source, File dest) throws IOException{
-		//to make sure this file is not used by the server atm (important for worlds)
-		ArrayList<String> skip = new ArrayList<String>(Arrays.asList("uid.dat","session.dat"));
-		if(!(skip.contains(source.getName()))){
-			if(source.isDirectory()){
-				if(!(dest.exists())){
-					dest.mkdirs();
-				}
-				String[] files = source.list();
-				for(String file : files){
-					File sourceFile = new File(source, file);
-					File destFile = new File(dest, file);
-					copyFile(sourceFile, destFile);	
-				}
-			} else{
-				InputStream in = new FileInputStream(source);
-				OutputStream out = new FileOutputStream(dest);
-				byte[] buffer = new byte[1024];
-				int len;
-				while((len = in.read(buffer))>0){
-					out.write(buffer, 0, len);
-				}
-				in.close();
-				out.close();
-			}
-		}
-	}
-	
-	public static void deleteDirectory(File directory){
-		for(File file : directory.listFiles()){
-			if(file.isDirectory()){
-				deleteDirectory(file);
-			} else{
-				file.delete();
-			}
-		}
-		directory.delete();
-	}
 }
