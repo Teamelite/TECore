0c8518091a77dd25dd2c34c05457ab0c58476e81
[LIAM] <liam@axanite.net>
2016-01-19 21:43:30 +0000

    Updated version number & push date

--- src/io/teamelite/core/utilities/CoreInformation.java
+++ src/io/teamelite/core/utilities/CoreInformation.java
@@ -1,8 +1,8 @@
 package io.teamelite.core.utilities;
 
 public class CoreInformation {
-	static private String version = "1.1.1";
-	static private String pushDate = "07/01/16";
+	static private String version = "1.1.2";
+	static private String pushDate = "19/01/16";
 	
 	public static String getVersion() {
 		return version;
